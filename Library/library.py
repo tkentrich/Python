383c3a336dda44a832ac1b47d3cba78ef19f6982
Kent Richardson <tkentrich@gmail.com>
2022-02-18 14:49:30 -0700

    Upgrade Library to python3

--- Library/library.py
+++ Library/library.py
@@ -2,24 +2,24 @@
 
 import os
 import sqlite3
-from ID3 import *
+# from ID3 import *
 
 dbfile = './music.db'
 conn = sqlite3.connect(dbfile)
 
 def selectAlbum(artistID):
-    print "Select an album:"
+    print("Select an album:")
 
 def selectArtist():
     showartists()
-    print "- - - - -"
-    print "Select an artist"    
+    print("- - - - -")
+    print("Select an artist"    )
     
 def showartists():
     cursor = conn.execute('SELECT oid, name FROM artist ORDER BY name')
     results = cursor.fetchall()
     for row in results:
-        print '{0}) {1}'.format(row[0], row[1])
+        print('{0}) {1}'.format(row[0], row[1]))
     
 def showalbums():
     artistID = selectArtist()
@@ -29,16 +29,16 @@ def showtracks():
     albumID = selectAlbum(artistID)
 
 def inputlibrary():
-    print "Input Library"
+    print("Input Library")
 
 def populatelibrary():
     response = raw_input("Enter mp3 root folder: ")
-    print "Walking: \"",response,"\""
+    print("Walking: \"",response,"\"")
     tree = os.walk(response)
     try:
         (a, arts, a) = tree.next()
         for artist in arts:
-            print "New artist ", artist
+            print("New artist ", artist)
             artname = raw_input("Enter artist name (def: {0}, skip, quit):".format(artist))
             if artname == "":
                 artname = artist
@@ -49,25 +49,25 @@ def populatelibrary():
             artfolder=response + os.sep + artist
             # (a, albs, a) = tree.next()
             # for album in albs:
-                # print "   New album ", album
+                # print("   New album ", album)
                 # (a, a, trks) = tree.next()
                 # for track in trks:
-                    # print "      New track ", track
+                    # print("      New track ", track)
             
     except StopIteration:
-        print "Iteration stopped"
+        print("Iteration stopped")
 
 def mainmenu():
     done = False
     while not done:
-        print "1) Show artists"
-        print "2) Show an artist's albums"
-        print "3) Show an album's tracks"
-        print "4) Input library info"
-        print "5) Populate library based on mp3s"
-        print ""
-        print "0) Quit"
-        print ""
+        print("1) Show artists")
+        print("2) Show an artist's albums")
+        print("3) Show an album's tracks")
+        print("4) Input library info")
+        print("5) Populate library based on mp3s")
+        print("")
+        print("0) Quit")
+        print("")
         response = int(input ("Enter choice: "))
         if response == 1:
             showartists()
@@ -82,8 +82,8 @@ def mainmenu():
         elif response == 0:
             done = True
         else:
-            print "Invalid response:", response
+            print("Invalid response:", response)
 
-    print "Good-Bye!"
+    print("Good-Bye!")
 
 mainmenu()
